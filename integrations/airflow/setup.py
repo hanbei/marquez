e67979017247ab7866cf4f9229b9071f6e508f19
Maciej Obuchowski <maciej.obuchowski@getindata.com>
2021-06-09 15:03:49 +0200

    dbt/integration
    Signed-off-by: Maciej Obuchowski <maciej.obuchowski@getindata.com>

--- integrations/airflow/setup.py
+++ integrations/airflow/setup.py
@@ -43,7 +43,7 @@ extras_require = {
         "apache-airflow[gcp_api]==1.10.12",
         "apache-airflow[google]==1.10.12",
         "apache-airflow[postgres]==1.10.12",
-        "snowflake-connector-python==2.4.3",
+        "snowflake-connector-python==2.2.9",
         "airflow-provider-great-expectations==0.0.6",
     ],
 }
